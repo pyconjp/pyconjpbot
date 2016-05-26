3df3aafd588c2f1dbe8bef965456818236b16f29
Takanori Suzuki <takanory@beproud.jp>
2016-05-26 18:15:39 +0900

    SAR-568 change manual command to respond_to

--- pyconjpbot/plugins/manual.py
+++ pyconjpbot/plugins/manual.py
@@ -5,15 +5,14 @@ from slackbot.bot import respond_to, listen_to
 # マニュアルのURL
 URL = 'http://manual.pycon.jp/'
 
-@listen_to('^manual$')
-@listen_to('^マニュアル$')
+@respond_to('manual$')
 def manual(message):
     """
     マニュアルのURLを返す
     """
     message.send('PyCon JP 運営マニュアル {}'.format(URL))
 
-@listen_to('^manual (.*)')
+@respond_to('manual (.*)')
 def manual_search(message, query):
     """
     マニュアルをキーワード検索したURLを返す
@@ -21,10 +20,10 @@ def manual_search(message, query):
     if query != 'help':
         message.send('{}search.html?q={}'.format(URL, quote_plus(query)))
 
-@listen_to('^manual help$')
+@respond_to('manual help$')
 def manual_help(message):
     """
     マニュアルコマンドのヘルプを返す
     """
-    message.send('''- `manual`: マニュアルのURLを返す
-- `manual keywords`: マニュアルを指定キーワード検索する''')
+    message.send('''- `$manual`: マニュアルのURLを返す
+- `$manual keywords`: マニュアルを指定キーワード検索する''')
