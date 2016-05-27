d0349608cbda88968ad5b3781383d4bc91562aa9
Takanori Suzuki <takanory@beproud.jp>
2016-05-27 14:49:53 +0900

    SAR-568 add shuffle, choice command

---
+++ pyconjpbot/plugins/shuffle.py
@@ -0,0 +1,21 @@
+import random
+
+from slackbot.bot import respond_to, listen_to
+
+@respond_to('shuffle (.*)')
+def shuffle(message, words):
+    words = words.split()
+    if len(words) == 1:
+        message.send('キーワードを複数指定してください\n`$shuffle word1 word2...`')
+    else:
+        random.shuffle(words)
+        message.send(' '.join(words))
+
+@respond_to('choice (.*)')
+def choice(message, words):
+    words = words.split()
+    if len(words) == 1:
+        message.send('キーワードを複数指定してください\n`$choice word1 word2...`')
+    else:
+        message.send(random.choice(words))
+        
