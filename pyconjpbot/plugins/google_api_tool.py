26bdbc2b0cfda00ed0cbfb62625c2f632d6adfe3
Takanori Suzuki <takanori@takanory.net>
2016-06-02 00:39:02 +0900

    SAR-691 remove main function from googl_api_tool

--- pyconjpbot/plugins/google_api_tool.py
+++ pyconjpbot/plugins/google_api_tool.py
@@ -7,8 +7,6 @@ from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
 
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
-
 def get_service(name, version, filename, scope):
     """指定された Google API に接続する
 
@@ -49,24 +47,3 @@ def get_service(name, version, filename, scope):
 
     service = discovery.build(name, version, http=http)
     return service
-
-def main():
-    """Shows basic usage of the Google Drive API.
-
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
-    """
-    service = get_service('drive', 'v3', __file__, SCOPES)
-
-    results = service.files().list(
-        pageSize=10,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
-
-if __name__ == '__main__':
-    main()
