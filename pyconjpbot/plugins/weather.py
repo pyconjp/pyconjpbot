810b393aa03c2d2c748844f36d7a3a15655037b4
Takanori Suzuki <takanory@beproud.jp>
2016-06-02 19:11:07 +0900

    SAR-700 add today and tomorrow weather

--- pyconjpbot/plugins/weather.py
+++ pyconjpbot/plugins/weather.py
@@ -19,7 +19,7 @@ def _get_forecast_text(forecast):
     telop = forecast['telop']
     temp = forecast['temperature']
     
-    text = '{}{}'.format(WEATHER_EMOJI.get(telop, ''), telop)
+    text = '{} は {}{}'.format(date, WEATHER_EMOJI.get(telop, ''), telop)
     if temp['min']:
         text += ' 最低気温{}℃'.format(temp['min']['celsius'])
     if temp['max']:
@@ -27,8 +27,9 @@ def _get_forecast_text(forecast):
 
     return text
 
-@respond_to('(weather|天気)')
-def weather(message, command):
+@respond_to('(weather|天気予報)')
+@respond_to('(weather|天気予報)\s+(.*)')
+def weather(message, command, place='東京'):
     """
     天気予報を返す
     """
@@ -36,13 +37,14 @@ def weather(message, command):
     data = r.json()
 
     city = data['location']['city']
-    time = parser.parse(data['publicTime'])
+    #time = parser.parse(data['publicTime'])
     link = data['link']
-    text = _get_forecasst_text(data['forecasts'][0])
+    text = _get_forecast_text(data['forecasts'][0]) + '\n'
+    text += _get_forecast_text(data['forecasts'][1])
 
     attachments = [{
-        'fallback': '{}の{}の天気'.format(city, date),
-        'pretext': '<{}|{}の{}の天気> ({:%m月%d日%H:%M}発表)'.format(link, city, date, time),
+        'fallback': '{}の天気'.format(city),
+        'pretext': '<{}|{}の天気>'.format(link, city),
         'text': text,
     }]
 
