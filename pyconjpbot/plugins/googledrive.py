dbca5204413f933a2b94af1e6fa76db73f67de46
Takanori Suzuki <takanori@takanory.net>
2016-06-04 20:58:13 +0900

    SAR-691 add folder ids for google drive

--- pyconjpbot/plugins/googledrive.py
+++ pyconjpbot/plugins/googledrive.py
@@ -1,15 +1,29 @@
 import json
 
 from slackbot.bot import respond_to
-from pyconjpbot.plugins import google_api_tool
+#from pyconjpbot.plugins import google_api_tool
+import google_api_tool
 
+# Google Drive API の Scope
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 
+# PyCon JP 関連の検索対象とする親フォルダの Id
 FOLDER = {
+    'PyCon JP': '0BzmtypRXAd8zZDZhOWJkNWQtMDNjOC00NjQ1LWI0YzYtZDU3NzY1NTY5NDM3',
+    '一社': '0B_bw8GEmTD5OQmxBZ1l6UzNodmc',
     '2016': '0B_bw8GEmTD5OTTl1U0pWeW43Sk0',
+    '全体': '0B_bw8GEmTD5OU1BWd0xBZWRkQkk',
+    '事務局': '0B_bw8GEmTD5OTDd6Z2NJWTd0RW8',
+    '会場': '0B_bw8GEmTD5OQjBpc00zRlo3RU0',
+    'プログラム': '0B_bw8GEmTD5OemhocVR1a1ViOFU',
+    'メディア': '0B_bw8GEmTD5OUmFFdVV1S0NKU2c',
+    '議事録': '0B_bw8GEmTD5OZEJxZ2pBc0NmUWs',
     '2015': '0B_bw8GEmTD5ORXAtU28xNC0tTDg',
     '2014': '0B_bw8GEmTD5OZ0FCNXlWSTEtOU0',
-    '一般社団法人': '0B_bw8GEmTD5OQmxBZ1l6UzNodmc',
+    '2013': '0BzmtypRXAd8zR3FQWkhKY3luajg',
+    '2012': '0B08rY0k8XSeVZjEzNzYyYjktNzc1Mi00N2Q0LTgxZjYtMzMyZjk2Yjg3ZDBl',
+    '2011': '0BzmtypRXAd8zNGRiNmQyYTUtZTFjMS00ZGYwLWJhMGItZWU3ZTEyYjJlMWU1',
+    'mini': '0BzmtypRXAd8zODdjODljOTctOWU5ZS00ZWJjLTk4MjgtNDMyZDExODA1NzQ0',
     }
 
 MIME_TYPE = {
@@ -54,7 +68,7 @@ def main():
     service = google_api_tool.get_service('drive', 'v3', __file__, SCOPES)
 
     results = service.files().list(
-        pageSize=10, fields="nextPageToken, files", q='fullText contains "名簿" and "{}" in parents'.format(FOLDER['2016'])).execute()
+        pageSize=10, fields="nextPageToken, files", q='fullText contains "名簿" and "{}" in parents or "{}" in parents'.format(FOLDER['2016'], FOLDER['2015'])).execute()
     for item in results.get('files', []):
         print(item['name'], item['id'])
         print(item['webViewLink'])
