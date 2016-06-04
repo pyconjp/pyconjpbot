81136016eb20176c072c0825ce7e5f3e879b2e8c
Takanori Suzuki <takanori@takanory.net>
2016-06-04 21:58:33 +0900

    SAR-691 merge google_api_tool.py to googledrive.py

--- pyconjpbot/plugins/googledrive.py
+++ pyconjpbot/plugins/googledrive.py
@@ -1,8 +1,13 @@
+import argparse
+import httplib2
+import os
 import json
 
 from slackbot.bot import respond_to
-#from pyconjpbot.plugins import google_api_tool
-import google_api_tool
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
 
 # Google Drive API の Scope
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
@@ -34,7 +39,50 @@ MIME_TYPE = {
     'application/vnd.google-apps.form': 'フォーム',
     }
 
-service = google_api_tool.get_service('drive', 'v3', __file__, SCOPES)
+    
+def get_service(name, version, filename, scope):
+    """指定された Google API に接続する
+
+    name: APIの名前
+    version: APIのバージョン(通常 v3)
+    file: ファイルの場所を指定する、通常 __file__ を使用する
+    scope: OAuth のスコープを指定する
+
+    serviceオブジェクトを返す
+    """
+
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+
+    # Name of a file containing the OAuth 2.0 information for this
+    # application, including client_id and client_secret, which are found
+    # on the API Access tab on the Google APIs
+    # Console <http://code.google.com/apis/console>.
+    client_secrets = os.path.join(os.path.dirname(filename),
+                                  'client_secrets.json')
+
+    # Set up a Flow object to be used if we need to authenticate.
+    flow = client.flow_from_clientsecrets(
+        client_secrets,
+        scope=scope,
+        message=tools.message_if_missing(client_secrets))
+
+    # Prepare credentials, and authorize HTTP object with them.
+    # If the credentials don't exist or are invalid run through the native
+    # client flow. The Storage object will ensure that if successful the good
+    # credentials will get written back to a file.
+    storagefile = os.path.join(os.path.dirname(filename),
+                               name + '.dat')
+    storage = file.Storage(storagefile)
+    credentials = storage.get()
+    if credentials is None or credentials.invalid:
+        credentials = tools.run_flow(flow, storage, flags)
+    http = credentials.authorize(http = httplib2.Http())
+
+    service = discovery.build(name, version, http=http)
+    return service
+
+# APIに接続する
+service = get_service('drive', 'v3', __file__, SCOPES)
 
 @respond_to('drive (.*)')
 def drive_search(message, keywords):
@@ -65,7 +113,7 @@ def main():
     """
     Google Drive APIを認証して使用できるようにする
     """
-    service = google_api_tool.get_service('drive', 'v3', __file__, SCOPES)
+    service = get_service('drive', 'v3', __file__, SCOPES)
 
     results = service.files().list(
         pageSize=10, fields="nextPageToken, files", q='fullText contains "名簿" and "{}" in parents or "{}" in parents'.format(FOLDER['2016'], FOLDER['2015'])).execute()
