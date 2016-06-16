7b9f69e088756889216b01524c78a2028d599d1e
Takanori Suzuki <takanory.net@gmail.com>
2016-06-16 10:43:13 +0900

    SAR-716 add _get_user_name method for mentions plus plus

--- pyconjpbot/plugins/plusplus.py
+++ pyconjpbot/plugins/plusplus.py
@@ -1,8 +1,34 @@
 from slackbot.bot import listen_to
+from slackbot import settings
+import slacker
 
-@listen_to(r'@?([^@]*[^-:]):?\s+(\+\+|--)')
+def _get_user_name(user_id):
+    """
+    指定された Slack の user_id に対応する username を返す
+
+    Slacker で users.list API を呼び出す
+    - https://github.com/os/slacker
+    - https://api.slack.com/methods/users.info
+    """
+    webapi = slacker.Slacker(settings.API_TOKEN)
+    response = webapi.users.info(user_id)
+    if response.body['ok']:
+        return response.body['user']['name']
+    else:
+        return ''
+
+@listen_to(r'(@?[^@]*[^:+-]):?\s*(\+\+|--)')
 def plusplus(message, target, plusplus):
     """
     指定された名前に対して ++ または -- する
+    takanory++
+    takanory:++
+    takanory: ++
+    @takanory++
+    @takanory:++
+    @takanory: ++
     """
-    message.send('{} {}'.format(target, plusplus))
+    import pdb
+    # 名前が <@userid> でくる
+    pdb.set_trace()
+    message.send('name: {}, plusplus: {}'.format(target, plusplus))
