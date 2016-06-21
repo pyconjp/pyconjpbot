d43f1bbb05a98154774cfae7d7c0e664f1cb9b0e
Takanori Suzuki <takanori@takanory.net>
2016-06-22 00:22:15 +0900

    SAR-870 change reaction plugin based on REACTION dict

--- pyconjpbot/plugins/reaction.py
+++ pyconjpbot/plugins/reaction.py
@@ -3,47 +3,45 @@ import re
 from slackbot.bot import listen_to
 from slacker import Error
 
-def _react(message, emoji):
-    try:
-        message.react(emoji)
-    except Error as error:
-        # 同じリアクションをすると例外が発生するので、無視する
-        if error.args[0] == 'already_reacted':
-            pass
+# リアクション対象のキーワードと絵文字
+REACTION = {
+    ('肉', 'meat'): 'meat_on_bone',
+    'カレーメシ': ('curry', 'boom'),
+    ('ピザ', 'pizza'): 'pizza',
+    ('sushi', '寿司', 'おすし'): 'sushi',
+    '酒': 'sake',
+    ('ビール', 'beer'): 'beer',
+    'さくさく': 'panda_face',
+}
+
+def _react(message, emojis):
+    """
+    指定された emoji を reaction で返す
+    """
+    if isinstance(emojis, str):
+        # tuple に変換する
+        emojis = (emojis, )
+    for emoji in emojis:
+        try:
+            message.react(emoji)
+        except Error as error:
+            # 同じリアクションをすると例外が発生するので、無視する
+            if error.args[0] == 'already_reacted':
+                pass
+            else:
+                raise
+
+@listen_to('(.*)')
+def reaction(message, text):
+    """
+    メッセージの中にリアクションする文字列があれば、emojiでリアクションする
+    """
+    text = text.lower()
+    for words, emojis in REACTION.items():
+        if isinstance(words, str):
+            if words in text:
+                _react(message, emojis)
         else:
-            raise
-
-@listen_to('肉')
-@listen_to('meat', re.IGNORECASE)
-def react_niku(message):
-    _react(message, 'meat_on_bone')
-
-@listen_to('酒')
-def react_niku(message):
-    _react(message, 'sake')
-    
-@listen_to('ビール')
-@listen_to('beer', re.IGNORECASE)
-def react_niku(message):
-    _react(message, 'beers')
-    
-@listen_to('すし')
-@listen_to('寿司')
-@listen_to('sushi')
-def react_niku(message):
-    _react(message, 'sushi')
-    
-@listen_to('ピザ')
-@listen_to('pizza', re.IGNORECASE)
-def react_niku(message):
-    _react(message, 'pizza')
-    
-@listen_to('カレーメシ')
-def react_niku(message):
-    _react(message, 'curry')
-    _react(message, 'boom')
-    
-@listen_to('さくさく')
-def react_niku(message):
-    _react(message, 'panda_face')
-    
+            # 正規表現で指定した単語が存在するかチェックする
+            if re.search('|'.join(words), text):
+                _react(message, emojis)
