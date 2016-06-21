02e322ddd5f122b3cc25e22811678bbea46aaced
Takanori Suzuki <takanory.net@gmail.com>
2016-06-21 18:39:44 +0900

    SAR-866 update calc plugin for ignore number only string

--- pyconjpbot/plugins/calc.py
+++ pyconjpbot/plugins/calc.py
@@ -1,11 +1,20 @@
+import re
+
 from slackbot.bot import respond_to, listen_to
 from sympy import sympify, SympifyError
 
-@listen_to('^(([-+*/^%()\d\s]|pi|e|sqrt|sin|cos|tan)+)$')
+# 単一の数字っぽい文字列を表すパターン
+NUM_PATTERN = re.compile('^\s*[-+]?[\d.]+\s*$')
+
+@listen_to('^(([-+*/^%!()\d\s]|pi|e|sqrt|sin|cos|tan)+)$')
 def calc(message, expression, dummy_):
     """
     数式っぽい文字列だったら計算して結果を返す
     """
+
+    # 数字っぽいパターンの場合は無視する
+    if NUM_PATTERN.match(expression):
+        return
     try:
         result = sympify(expression)
     except SympifyError:
