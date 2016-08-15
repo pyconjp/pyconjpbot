bcc0e79b6840508787c3d361fb042feeb5a37949
Takanori Suzuki <takanory.net@gmail.com>
2016-08-15 18:13:07 +0900

    SAR-988 add search and help command to github plugin

--- pyconjpbot/plugins/github.py
+++ pyconjpbot/plugins/github.py
@@ -14,7 +14,7 @@ def github_repos(message):
     """
     text = ""
     for repo in org.get_repos():
-        text += "- <{}|{}> {}\n".format(repo.url, repo.name, repo.description)
+        text += "- <{}|{}> {}\n".format(repo.html_url, repo.name, repo.description)
 
     attachments = [{
         'pretext': '{} のリポジトリ一覧'.format(settings.GITHUB_ORGANIZATION),
@@ -23,3 +23,38 @@ def github_repos(message):
     }]
     message.send_webapi('', json.dumps(attachments))
 
+@respond_to('^github\s+search\s+(.*)')
+def github_search(message, keywords):
+    """
+    指定されたキーワードでissueを検索する
+    """
+
+    text = ""
+    for repo in org.get_repos():
+        # リポジトリを指定して検索
+        issues = list(github.search_issues(keywords, repo=repo.full_name))
+        if issues:
+            text += "リポジトリ: <{}|{}>\n".format(repo.html_url, repo.name)
+            for issue in issues:
+                text += "- <{}|{}>\n".format(issue.html_url, issue.title)
+
+    if text:
+        attachments = [{
+            'pretext': '`{}` の検索結果'.format(keywords),
+            'text': text,
+            'mrkdwn_in': ['pretext', 'text'],
+        }]
+        message.send_webapi('', json.dumps(attachments))
+    
+    # 結果が一つもない場合
+    else:
+        message.send('`{}` にマッチするissueはありません'.format(keywords))
+
+@respond_to('^github\s+help$')
+def github_help(message):
+    """
+    githubコマンドのヘルプを返す
+    """
+    message.send('''- `$gihub repos`: pyconjp organization のリポジトリ一覧を返す
+- `$github search keywords`: 指定されたキーワードにマッチするissueを返す
+''')
